e03312fc8e1a6a4d8fb3f28e934f28ea5c0905f0
lizy <77211356@qq.com>
2018-09-25 10:08:26 +0800

    加载要素服务测试
    加载要素服务测试

--- js/LeafletMap.js
+++ js/LeafletMap.js
@@ -3,23 +3,27 @@ var layer;
 var layerLabels;
 //地图初始化
 function init(){
-	//map = L.map('map').setView([30.56486,114.353622 ], 11);  
-	map = L.map('map').setView([45.526, -122.667], 13);
+	map = L.map('map').setView([30.56486,114.353622 ], 11);  
+	//map = L.map('map').setView([45.526, -122.667], 13);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
 	
-	  var trees = L.esri.featureLayer({
-	    url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0'
-	  }).addTo(map);
+	loadWFS("urbanlayer:HubeiBoundary","EPSG:3857")
+	
+//	  var trees = L.esri.featureLayer({
+//	    url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0'
+//	  }).addTo(map);
+	
+	//http://localhost:8080/geoserver/urbanlayer/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=urbanlayer:HubeiBoundary&maxFeatures=50&outputFormat=application%2Fjson
 
-	//zoom in all features
-	trees.query().bounds(function (error, latlngbounds) {
-	    map.fitBounds(latlngbounds);
-	  });
-
-	//popup windows
-	trees.bindPopup(function (layer) {
-	    return L.Util.template('<p>{COMMON_NAM}<br>{SCIENTIFIC}<br>{NOTES}</p>', layer.feature.properties);
-	});
+//	//zoom in all features
+//	trees.query().bounds(function (error, latlngbounds) {
+//	    map.fitBounds(latlngbounds);
+//	  });
+//
+//	//popup windows
+//	trees.bindPopup(function (layer) {
+//	    return L.Util.template('<p>{COMMON_NAM}<br>{SCIENTIFIC}<br>{NOTES}</p>', layer.feature.properties);
+//	});
 	
 	
 	//	L.esri.tiledMapLayer({
@@ -78,3 +82,42 @@ function setBasemap(basemap) {
     var basemap = basemaps.value;
     setBasemap(basemap);
  }
+ 
+ 
+ //加载WFS服务
+function loadWFS(layerName,epsg){
+			var param = {
+					service:'WFS',
+					version:'1.0.0',
+					request:'GetFeature',
+					typeName:layerName,
+					outputFormat:'application/json',
+					srsName:epsg
+			};
+			var rootWFS= 'http://localhost:8080/geoserver/urbanlayer/ows?';
+			var u = rootWFS + L.Util.getParamString(param,rootWFS);
+			$.ajax({
+				url: u, 
+				dataType:'json',
+				success:loadWfsHandler,
+			});
+			var layer;
+			function loadWfsHandler(data){
+				console.log(data);
+				layer = L.geoJson(data,{
+// 					style:function(feature){
+// 						return {
+// 							stroke:true,
+// 							color:'#F80909',
+// 							opacity: 1,
+// 			                fillOpacity: 0.9,
+// 			                fillColor: '#F80909',
+// 							weight:5
+// 						}
+// 					},
+					pointToLayer:function(featyre,latlng){
+						
+					}
+				}).addTo(map);
+			}
+	} 
